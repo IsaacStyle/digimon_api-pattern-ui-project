576d677029731d84be404da23094818739a302ca
IsaacStyle <isaac.opra@gmail.com>
2023-01-11 11:11:36 -0500

    Fix Navbar and logos

--- Projects/api-react-ui-project/src/Nav.jsx
+++ Projects/api-react-ui-project/src/Nav.jsx
@@ -1,8 +1,12 @@
 function NAV() {
     return(
+        <>
+        <h1 className='placeholder'>placeholder</h1>
         <nav className='top'>
-            <h1 className='title'>Digimon Gallery</h1>
+            <h1 className='title'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Digimon_Logo.svg/1200px-Digimon_Logo.svg.png' alt='Digimon Gallery' className='toplogo2'></img>
+            <span><img src='https://www.pngall.com/wp-content/uploads/2/Digimon-Logo-PNG-Free-Download.png' alt='Digimon Gallery' className='toplogo'></img></span></h1>
         </nav>
+        </>
     )
 }
 export default NAV
